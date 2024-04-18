2fece868a0583b960613698507da0e386ff231cc
vikas shukla <vikasshuklaaf18@gmail.com>
2024-04-18 23:27:50 +0530

    sdfe

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://6620b084fab17a022263932e--gregarious-kleicha-3dd519.netlify.app/",
+    origin: "http://localhost:5173/",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
